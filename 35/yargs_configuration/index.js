2b26fd6c4b8df65c1934b8135f765b5014a1f8fc
Juan Camilo <juanccamachob94@gmail.com>
2022-05-11 02:10:09 -0500

    send promise as parameter

--- 35/yargs_configuration/index.js
+++ 35/yargs_configuration/index.js
@@ -1,17 +1,26 @@
-const colors = require('colors/safe');
+const safeColors = require('colors/safe');
+const colors = require('colors');
 const { argv } = require('./helpers/yargs_argv');
 const { createFile } = require('./helpers/file_creator')
 
 const testMethod = async(value) => {
   return value.toString();
 }
-// evidence try,catch(err) { throw err; } is not neccessary
-const validateThrowError = () => {
-  testMethod(null)
-    .then(result => console.log(colors.blue(`result: ${result}`)))
-    .catch(err => console.log(colors.red(`catch error: ${err}`)));
+
+const runSimplePromise = async (simplePromise) => {
+  simplePromise
+    .then(result => console.log(safeColors.blue(`result: ${result}`)))
+    .catch(err => console.log(safeColors.red(`catch error: ${err}`)));
 }
 
-createFile(argv.base, argv.enabledLog)
-  .then(result => console.log(colors.blue(`result: ${result}`)))
-  .catch(err => console.log(colors.red(`catch error: ${err}`)));
+// evidence try,catch(err) { throw err; } is not neccessary
+const validateThrowError = () => {
+  runSimplePromise(testMethod(null));
+}
+
+const runFileCreation = () => {
+  runSimplePromise(createFile(argv.base, argv.enabledLog));
+}
+
+console.log('Init');
+runFileCreation();
