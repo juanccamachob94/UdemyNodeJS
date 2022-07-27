24dc9393c2381c97fe4ef99e9340f6eca4a03b5d
Juan Camilo <juanccamachob94@gmail.com>
2022-07-27 12:10:53 -0500

    handlebars

--- section7/webserver/app.js
+++ section7/webserver/app.js
@@ -1,13 +1,29 @@
+const hbs = require('hbs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
+
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  // res.render('home');
+  res.render('home', {
+    name: 'Pedro Pérez',
+    title: 'Node Course'
+  });
+});
+
+
+app.get('/generic', (req, res) => {
+  res.render('generic');
+});
+
+app.get('/elements', (req, res) => {
+  res.render('elements');
 });
 
 app.get('/:resource', (req, res) => {
